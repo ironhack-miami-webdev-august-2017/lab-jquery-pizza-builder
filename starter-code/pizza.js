058835791c30369b2152714f830e2765acec319a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-08-18 09:22:04 -0400

    Toggle ingredients (no price)

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,31 @@
-// Write your Pizza Builder JavaScript in this file.
+$(document).ready(function () {
+    $('.btn-pepperonni').click(function () {
+        $('.btn-pepperonni').toggleClass('active');
+
+        $('.pep').fadeToggle();
+    });
+
+    $('.btn-mushrooms').click(function () {
+        $('.btn-mushrooms').toggleClass('active');
+
+        $('.mushroom').fadeToggle();
+    });
+
+    $('.btn-green-peppers').click(function () {
+        $('.btn-green-peppers').toggleClass('active');
+
+        $('.green-pepper').fadeToggle();
+    });
+
+    $('.btn-sauce').click(function () {
+        $('.btn-sauce').toggleClass('active');
+
+        $('.sauce').toggleClass('sauce-white');
+    });
+
+    $('.btn-crust').click(function () {
+        $('.btn-crust').toggleClass('active');
+
+        $('.crust').toggleClass('crust-gluten-free');
+    });
+});
